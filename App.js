c47e1710f874e19c49f3b33f02384aaba4943d3c
Shreya-1206 <shreyaray.1206@gmail.com>
2024-05-25 22:37:40 +0530

    lay foundation react

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom"
+
+//react element
 const parent = React.createElement('h1', {id :"heading"}, "Hello From  React 😃");
 
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+//jsx element
+const jsxHeading = <h1 id = "2" className="helloJsx" >Hello From JSX ☺️</h1>;
+console.log(jsxHeading); //jsx -- react.createElement(obj) -- htmlelemnt
+
+root.render(jsxHeading);
